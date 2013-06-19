83135f919a643c3ce9263e4247c4afc3614cda34
cbernat <cbernat@makingsense.com>
2013-06-19 17:46:32 -0300

    Added changes to library.

--- js/player.js
+++ js/player.js
@@ -1,30 +1,69 @@
 var player = 
 {
-	
-	initialize: function (containerId){
-		var playerObject = $("<audio/>", {id :'musicPlayer'});
-		$("#"+containerId).append(playerObject);
-	},
-	
-	
-	addToPlaylist: function (song)
-	{
-		if ($("#musicPlayer").length > 0)
-		{
-			var playerObject = $("#musicPlayer");
-			var source= $('<source/>');
-			if (playerObject[0].canPlayType('audio/mpeg;')) {
-				$(source).attr("type", "audio/mpeg");
-				$(source).attr("media",+song+'.mp3');
-			} else {
-				$(source).attr("type", 'audio/ogg');
-				$(source).attr("src", 'media/'+song+'.ogg');
+	playerObject: $("<audio/>", {id :'musicPlayer'}),
+	currentTrack:0,
+	trackCount:0,
+	repeatPlayList: false,
+	playlistSources: "",
+	initialize: function (){
+		
+		this.playerObject = $("<audio/>", {id :'musicPlayer'});
+		this.playlistSources = new Array();
+		$(this.playerObject).on('ended',function(){
+		
+			if(this.currentTrack==this.trackCount-1){
+				if(this.repeatPlayList){
+					this.currentTrack = 0;
+					this.playerObject.src = this.playlistSources[this.currentTrack];
+					this.Play();
+				}
+			}else{
+				this.currentTrack++;
+				this.playerObject.src = this.playlistSources[this.currentTrack];
+				this.Play();
 			}
-			$(playerObject).append(source);
+		});
+		this.currentTrack=0;
+		if (!(!!(this.playerObject.canPlayType) && ("no" != this.playerObject.canPlayType("audio/mpeg")) && ("" != this.playerObject.canPlayType("audio/mpeg")))) {
+			//Add flash fallback here
 		}
 	},
+
+	addToPlaylist: function (song)
+	{
+		if ($(this.playerObject).length > 0)
+		{
+			var source= $('<source/>');
+			if (this.playerObject[0].canPlayType('audio/mpeg;')) {
+				$(source).attr("src", song);
+				this.playlistSources[this.trackCount] = song;
+				$(source).attr("type", "audio/mpeg");
+			} 
+			$(this.playerObject).append(source);
+			this.trackCount++;
+		}
+	},
+	
 	Play: function(){
-		$("#musicPlayer")[0].play();
+		this.playerObject[0].play();
+	},
+	
+	Next: function(){
+		if(this.currentTrack==this.trackCount-1){
+				if(this.repeatPlayList){
+					this.currentTrack = 0;
+					this.playerObject.src = this.playlistSources[this.currentTrack];
+					this.playerObject[0].load();
+					this.Play();
+				}
+			}else{
+				this.currentTrack++;
+				this.playerObject.src = this.playlistSources[this.currentTrack];
+				this.playerObject[0].load();
+				this.Play();
+			}
 	}
+	
+	
 	
 }	
